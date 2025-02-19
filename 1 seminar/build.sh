098411d0516d78fcc37cb72d9fcf8b300c60438e
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2025-02-19 09:03:28 +0700

    Add targets in script to reformat sources and make install dir

--- 1 seminar/build.sh
+++ 1 seminar/build.sh
@@ -4,5 +4,7 @@ cd build
 conan install .. --output-folder=. --build=missing
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build . --target=docs
+cmake --build . --target=format
 cmake --build .
+cmake --install . --prefix ./install
 ctest -V -j4
